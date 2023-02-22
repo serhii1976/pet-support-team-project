f3855d6788f1664f7e731c9f258d321ffa35ee56
Serhii Zhyhanov <zhiganov76@gmail.com>
2023-02-22 20:31:25 +0200

    init

--- src/components/AddNoticeButton/AddNoticeButton.js
+++ src/components/AddNoticeButton/AddNoticeButton.js
@@ -8,19 +8,19 @@ import { ToastContainer, toast } from 'react-toastify';
 import toastAuthNeeded from '../../utils/toastAuthNeeded';
 
 const { MobBtn, Text, Btn, Cross, MobCross } = elements;
-const { selectIsLoggedIn } = authSelectors
+const { selectIsLoggedIn } = authSelectors;
 
 const AddNoticeButton = () => {
   const [active, setActive] = useState(false);
-  const loggedIn = useSelector(selectIsLoggedIn)
+  const loggedIn = useSelector(selectIsLoggedIn);
 
   const handleActivation = () => {
     if (!loggedIn) {
-      return toast.info("For this operation registration or login needed", toastAuthNeeded)
+      return toast.info('For this operation registration or login needed', toastAuthNeeded);
     }
 
-    setActive(true)
-  }
+    setActive(true);
+  };
 
   return (
     <div style={{ flexShrink: 0 }}>
@@ -40,4 +40,5 @@ const AddNoticeButton = () => {
     </div>
   );
 };
+
 export default AddNoticeButton;
